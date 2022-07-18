912fdb6b3f0cf5d10b8753a104291d9c9d7d7192
christian <christian.villanueva@correounivalle.edu.co>
2022-07-18 15:32:22 -0500

    puerto en variable de entorno

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+const database = require('./src/config/database');
+const userRoutes = require('./src/routes/routes');
+
+// middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// endpoints: A PARTIR DE EL COMANDO /user se van a enlistar todas las rutas que tienen logica
+// endpoints
+app.use('/user', userRoutes);
+
+// parse application/json
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file!
+  app.use(express.static('cliente/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'cliente', 'build', 'index.html'));
+  });
+  console.log("cliente iniciado");
+}
+
+//ENCARGADO DE HACER LA CONEXION DEL SERVIDOR Y DE
+const main = () => {
+  database.connect((err) => {
+    if (err) throw err;
+    console.log('Base de datos conectada');
+  });
+  const PORT = process.env.PORT || 5000;
+  console.log(PORT);
+
+  app.listen(PORT, () => {
+    console.log('Servidor escuchando en puerto ' + PORT);
+  });
+};
+
+main();
