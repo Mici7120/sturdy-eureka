6fb5e5316f91c4bf955acf06678f3ea820ddc264
christian <christian.villanueva@correounivalle.edu.co>
2022-07-18 18:54:08 -0500

    procfile npm start

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use('/user', userRoutes);
 // parse application/jsoncle
 app.use(bodyParser.json());
 
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
   app.use(express.static('cliente/build'));
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'cliente', 'build', 'index.html'));
   });
   console.log("cliente iniciado");
-}
+//}
 
 //ENCARGADO DE HACER LA CONEXION DEL SERVIDOR Y DE
 const main = () => {
